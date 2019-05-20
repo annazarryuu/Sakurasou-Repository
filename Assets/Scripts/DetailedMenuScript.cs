62f4e4895b3f244b6a5fbbe66900629c26a182f7
annazarryuu <annazar.ryuu@gmail.com>
2019-05-20 17:07:29 +0700

    Add in game menu

---
+++ Assets/Scripts/DetailedMenuScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class DetailedMenuScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject[] toBeHide;
+    public GameObject[] toBeUnhide;
+    private bool mutex = false;
+
+    private void switchMenu()
+    {
+        for(int i = 0; i < toBeHide.Length; i++)
+        {
+            toBeHide[i].SetActive(false);
+        }
+
+        for(int i = 0; i < toBeUnhide.Length; i++)
+        {
+            toBeUnhide[i].SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !(mutex))
+            switchMenu();
+    }
+
+    public void goToMainMenu()
+    {
+         SceneManager.LoadScene("MainMenu");
+    }
+    
+    public void OnPointerEnter (PointerEventData eventData)
+    {
+        mutex = true;
+    }
+
+    public void OnPointerExit (PointerEventData eventData)
+    {
+        mutex = false;
+    }
+}
---
+++ Assets/Scripts/InGameHoverTextChange.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InGameHoverTextChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Text textButton;
+    private Button button;
+
+    void Start()
+    {
+        textButton = GetComponentInChildren<Text>();
+        button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(textToPink);
+    }
+
+    public void textToWhite()
+    {
+        float r = 255f / 255f;
+        float g = 255f / 255f;
+        float b = 255f / 255f;
+        textButton.color = new Color(r, g, b, 1f);
+    }
+
+    public void textToPink()
+    {
+        float r = 250f / 255f;
+        float g = 101f / 255f;
+        float b = 147f / 255f;
+        textButton.color = new Color(r, g, b, 1f);
+    }
+
+    public void OnPointerEnter (PointerEventData eventData)
+    {
+        textToWhite();
+    }
+
+    public void OnPointerExit (PointerEventData eventData)
+    {
+        textToPink();
+    }
+}
